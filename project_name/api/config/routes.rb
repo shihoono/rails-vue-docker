d611d488372605d25af94d5954b06abc33bd2083
shihoono <shiho1449@gmail.com>
2021-09-24 17:12:01 +0900

    Posts CRUD

--- project_name/api/config/routes.rb
+++ project_name/api/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
+
+  scope format: 'json' do
+    resources :posts
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- project_name/api/db/schema.rb
+++ project_name/api/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_072437) do
+ActiveRecord::Schema.define(version: 2021_09_24_073803) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -42,4 +51,5 @@ ActiveRecord::Schema.define(version: 2021_09_24_072437) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
