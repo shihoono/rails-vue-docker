8d3d70150758e33f428f92a21d31b204b533835a
shihoono <shiho1449@gmail.com>
2021-09-28 11:42:14 +0900

    2要素認証設定ページの設定

--- project_name/api/config/routes.rb
+++ project_name/api/config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
   scope format: 'json' do
-    resources :posts
+    namespace :users do
+      resource :account, only: :show
+    end
+
+    resources :users do
+      resources :posts
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
